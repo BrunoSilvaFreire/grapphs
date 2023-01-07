0d17deb13adfd8dc8bfc0ce5cc7ed77b8a962b97
BrunoSilvaFreire <brunorbsf@gmail.com>
2023-01-07 18:16:31 -0300

    Fix some static adjacency matrix bad naming

--- include/grapphs/static_adjacency_matrix.h
+++ include/grapphs/static_adjacency_matrix.h
@@ -10,56 +10,63 @@
 #include <stdexcept>
 
 namespace gpp {
-    template<typename V, typename E, size_t Size, typename GraphIndex = default_graph_index>
-    class static_adjacency_matrix : public graph<V, E, GraphIndex> {
+    template<typename t_vertex, typename t_edge, size_t Size, typename t_index = default_graph_index>
+    class static_adjacency_matrix : public graph<t_vertex, t_edge, t_index> {
+    public:
+
+        using vertex_type = typename graph<t_vertex, t_edge, t_index>::vertex_type;
+        using edge_type = typename graph<t_vertex, t_edge, t_index>::edge_type;
+        using index_type = typename graph<t_vertex, t_edge, t_index>::index_type;
+
     private:
-        std::array<V, Size> _vertices;
-        std::array<E, Size * Size> _edges;
+        std::array<vertex_type, Size> _vertices;
+        std::array<t_edge, Size * Size> _edges;
+
     public:
         static_adjacency_matrix() : _vertices(), _edges() {
 
         }
 
         template<typename D>
-        explicit static_adjacency_matrix(graph<V, E, D>* other) : static_adjacency_matrix() {
-            for (GraphIndex i = 0; i < Size; ++i) {
-                V vertex;
+        explicit static_adjacency_matrix(graph<vertex_type, t_edge, D>* other) : static_adjacency_matrix() {
+            for (index_type i = 0; i < Size; ++i) {
+                vertex_type vertex;
                 if (other->try_get_vertex(i, vertex)) {
                     _vertices[i] = vertex;
                 }
             }
-            for (GraphIndex x = 0; x < Size; ++x) {
-                for (GraphIndex y = 0; y < Size; ++y) {
-                    E edge;
+            for (index_type x = 0; x < Size; ++x) {
+                for (index_type y = 0; y < Size; ++y) {
+                    t_edge edge;
                     if (other->try_get_edge(x, y, edge)) {
-                        GraphIndex i = index(x, y);
+                        index_type i = index(x, y);
                         _edges[i] = edge;
                     }
                 }
             }
         }
 
-        GraphIndex size() override {
+        index_type size() override {
             return Size;
         }
 
-        V* vertex(GraphIndex index) override {
-            return static_cast<V*>(&_vertices[index]);
+        vertex_type* vertex(index_type index) override {
+            return static_cast<vertex_type*>(&_vertices[index]);
         }
 
-        GraphIndex index(GraphIndex from, GraphIndex to) {
+        index_type index(index_type from, index_type to) {
             return from * _vertices.size() + to;
         }
 
-        E* edge(GraphIndex from, GraphIndex to) override {
+        t_edge* edge(index_type from, index_type to) override {
             return &_edges[index(from, to)];
         }
 
-        void connect(GraphIndex from, GraphIndex to, E edge) override {
+        void connect(index_type from, index_type to, t_edge edge) override {
             _edges[index(from, to)] = edge;
         }
 
-        std::vector<std::pair<const GraphIndex, E>> edges_from(GraphIndex vertex) const override {
+        std::vector<std::pair<const index_type, t_edge>> edges_from(index_type vertex) const override {
             throw std::runtime_error("Not implemented yet");
         }
     };
