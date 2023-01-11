535baf64210d817fdc3b37f05bac031a5adc1232
BrunoSilvaFreire <brunorbsf@gmail.com>
2023-01-11 20:08:39 -0300

    Better support for multi config generators

--- cmake/GrapphsConan.cmake
+++ cmake/GrapphsConan.cmake
@@ -35,22 +35,46 @@ function(grapphs_try_run_conan_install)
     list(APPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR})
     list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR})
 
+    get_property(is_multi_config GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+    if(is_multi_config)
+        set(conan_generator cmake_multi)
+    else()
+        set(conan_generator cmake)
+    endif()
+
     conan_cmake_configure(
             REQUIRES
             gtest/1.11.0
             nlohmann_json/3.10.5
             readosm/1.1.0a
-            GENERATORS cmake
+            GENERATORS
+            ${conan_generator}
     )
     message(VERBOSE "Conan Settings: ${CONAN_SETTINGS}")
     if("${CONAN_SETTINGS}" STREQUAL "")
-        conan_cmake_autodetect(CONAN_SETTINGS BUILD_TYPE ${CMAKE_BUILD_TYPE})
+        if(is_multi_config)
+            foreach(TYPE ${CMAKE_CONFIGURATION_TYPES})
+                conan_cmake_autodetect(settings BUILD_TYPE ${TYPE})
+                conan_cmake_install(
+                        PATH_OR_REFERENCE .
+                        BUILD missing
+                        REMOTE conancenter
+                        SETTINGS ${settings}
+                )
+            endforeach()
+
+            include(${CMAKE_CURRENT_BINARY_DIR}/conanbuildinfo_multi.cmake)
+        else()
+            conan_cmake_autodetect(CONAN_SETTINGS BUILD_TYPE ${CMAKE_BUILD_TYPE})
+            conan_cmake_install(
+                    PATH_OR_REFERENCE .
+                    BUILD missing
+                    SETTINGS ${CONAN_SETTINGS}
+            )
+            include(${CMAKE_CURRENT_BINARY_DIR}/conanbuildinfo.cmake)
+        endif()
     endif()
-    conan_cmake_install(
-            PATH_OR_REFERENCE .
-            BUILD missing
-            SETTINGS ${CONAN_SETTINGS}
-    )
-    include(${CMAKE_CURRENT_BINARY_DIR}/conanbuildinfo.cmake)
+    message(VERBOSE "Conan Settings: ${CONAN_SETTINGS}")
+
     conan_basic_setup(TARGETS)
 endfunction()
