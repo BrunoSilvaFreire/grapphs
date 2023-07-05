992ea1551b66061251df4d3e727278832a0c4965
Bruno Silva Freire <brunorbsf@gmail.com>
2023-07-05 20:00:32 -0300

    Optimized Conan workflow with file structure updates
    Refactored Conan integration for better usability and maintainability. Introduced grapphs_run_conan_install instead of grapphs_try_run_conan_install to improve readability. Added new Conan file to samples directory to handle dependencies. This change was necessary to streamline the workflow and reduce redundant code segments. Updated references in CMakeLists.txt files in root, samples, and tests directory accordingly.

--- cmake/GrapphsConan.cmake
+++ cmake/GrapphsConan.cmake
@@ -1,3 +1,13 @@
+if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+    message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+    file(
+            DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/release/0.18/conan.cmake"
+            "${CMAKE_BINARY_DIR}/conan.cmake"
+            TLS_VERIFY ON
+    )
+endif()
+
+include(${CMAKE_BINARY_DIR}/conan.cmake)
 
 function(grapphs_check_should_run_conan_install OUTPUT)
     if(DEFINED CONAN_EXPORTED)
@@ -13,33 +23,22 @@ function(grapphs_check_should_run_conan_install OUTPUT)
     endif()
 endfunction()
 
-function(grapphs_try_run_conan_install CONANFILE)
-    grapphs_check_should_run_conan_install(SHOULD_RUN)
-
-    if(NOT SHOULD_RUN)
-        message(STATUS "Skipping conan install")
-        return()
-    endif()
-
-    if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
-        message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-        file(
-                DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/release/0.17/conan.cmake"
-                "${CMAKE_BINARY_DIR}/conan.cmake"
-                TLS_VERIFY ON
-        )
-    endif()
-
-    include(${CMAKE_BINARY_DIR}/conan.cmake)
+macro(grapphs_run_conan_install CONANFILE)
+#    grapphs_check_should_run_conan_install(SHOULD_RUN)
+#
+#    if(NOT SHOULD_RUN)
+#        message(STATUS "Skipping conan install")
+#        return()
+#    endif()
 
     list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_BINARY_DIR})
     list(APPEND CMAKE_PREFIX_PATH ${CMAKE_CURRENT_BINARY_DIR})
 
-    get_property(is_multi_config GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
-    if(is_multi_config)
-        set(conan_generator cmake_find_package_multi)
+    get_property(IS_MULTI_CONFIG GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+    if(IS_MULTI_CONFIG)
+        set(CONAN_GENERATOR cmake_find_package_multi)
     else()
-        set(conan_generator cmake_find_package)
+        set(CONAN_GENERATOR cmake_find_package)
     endif()
 
     message(VERBOSE "Conan Settings: ${CONAN_SETTINGS}")
@@ -68,17 +67,15 @@ function(grapphs_try_run_conan_install CONANFILE)
     #        list(APPEND CONAN_OPTIONS "samples=False")
     #    endif()
 
-    if(is_multi_config)
+    if(IS_MULTI_CONFIG)
         foreach(TYPE ${CMAKE_CONFIGURATION_TYPES})
-            conan_cmake_autodetect(settings BUILD_TYPE ${TYPE})
+            conan_cmake_autodetect(CONAN_SETTINGS BUILD_TYPE ${TYPE})
             conan_cmake_install(
                     PATH_OR_REFERENCE ${CONANFILE}
                     BUILD missing
-                    SETTINGS ${settings}
+                    SETTINGS ${CONAN_SETTINGS}
             )
         endforeach()
-
-        include(${CMAKE_CURRENT_BINARY_DIR}/conanbuildinfo_multi.cmake)
     else()
         conan_cmake_autodetect(CONAN_SETTINGS BUILD_TYPE ${CMAKE_BUILD_TYPE})
         conan_cmake_install(
@@ -86,9 +83,5 @@ function(grapphs_try_run_conan_install CONANFILE)
                 BUILD missing
                 SETTINGS ${CONAN_SETTINGS}
         )
-        #            include(${CMAKE_CURRENT_BINARY_DIR}/conanbuildinfo.cmake)
     endif()
-    message(VERBOSE "Conan Settings: ${CONAN_SETTINGS}")
-    list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_BINARY_DIR}")
-    set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}" PARENT_SCOPE)
-endfunction()
+endmacro()
